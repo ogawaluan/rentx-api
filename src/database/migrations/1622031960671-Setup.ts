96b43ee778fa5589d3971766c7a445ffc86e8f8d
Luan Ogawa <ogawa.luan@gmail.com>
2021-12-16 16:16:29 -0300

    chore: some fix in migration

--- src/database/migrations/1622031960671-Setup.ts
+++ src/database/migrations/1622031960671-Setup.ts
@@ -24,23 +24,11 @@ export default class Setup1622031960671 implements MigrationInterface {
         image varchar(255) NULL,
         email varchar(255) NULL,
         password varchar(100) NULL,
-        temporaryPassword varchar(100) NULL,
-        facebookId varchar(255) NULL,
-        facebookImage text NULL,
-        googleId varchar(255) NULL,
-        googleImage text NULL,
-        appleId varchar(255) NULL,
         roleId varchar(36) NOT NULL,
         INDEX IDX_204e9b624861ff4a5b26819210 (createdAt),
         INDEX IDX_51b8b26ac168fbe7d6f5653e6c (name),
         INDEX IDX_2a32f641edba1d0f973c19cc94 (email),
-        INDEX IDX_df199bc6e53abe32d64bbcf211 (facebookId),
-        INDEX IDX_7044ce3fd64239a415b643096d (googleId),
-        INDEX IDX_6c19d05a6bbce0d3a0c298c4f7 (appleId),
         UNIQUE INDEX IDX_97672ac88f789774dd47f7c8be (email),
-        UNIQUE INDEX IDX_f9740e1e654a5daddb82c60bd7 (facebookId),
-        UNIQUE INDEX IDX_f382af58ab36057334fb262efd (googleId),
-        UNIQUE INDEX IDX_60cea0d80c39eedaaaf5e21f17 (appleId),
         PRIMARY KEY (id)
       ) ENGINE=InnoDB`
     );
@@ -66,25 +54,7 @@ export default class Setup1622031960671 implements MigrationInterface {
       'DROP INDEX IDX_204e9b624861ff4a5b26819210 ON users'
     );
     await queryRunner.query(
-      'DROP INDEX IDX_6c19d05a6bbce0d3a0c298c4f7 ON users'
-    );
-    await queryRunner.query(
-      'DROP INDEX IDX_7044ce3fd64239a415b643096d ON users'
-    );
-    await queryRunner.query(
       'DROP INDEX IDX_2a32f641edba1d0f973c19cc94 ON users'
-    );
-    await queryRunner.query(
-      'DROP INDEX IDX_f9740e1e654a5daddb82c60bd7 ON users'
-    );
-    await queryRunner.query(
-      'DROP INDEX IDX_df199bc6e53abe32d64bbcf211 ON users'
-    );
-    await queryRunner.query(
-      'DROP INDEX IDX_f382af58ab36057334fb262efd ON users'
-    );
-    await queryRunner.query(
-      'DROP INDEX IDX_60cea0d80c39eedaaaf5e21f17 ON users'
     );
     await queryRunner.query(
       'DROP INDEX IDX_97672ac88f789774dd47f7c8be ON users'
--- src/models/User.ts
+++ src/models/User.ts
@@ -27,27 +27,6 @@ class User extends BaseModel {
   @Column({ length: 100, select: false, nullable: true })
   password?: string;
 
-  @Column({ length: 100, select: false, nullable: true })
-  temporaryPassword?: string;
-
-  @Index()
-  @Column({ unique: true, nullable: true })
-  facebookId?: string;
-
-  @Column({ type: 'text', nullable: true })
-  facebookImage?: string;
-
-  @Index()
-  @Column({ unique: true, nullable: true })
-  googleId?: string;
-
-  @Column({ type: 'text', nullable: true })
-  googleImage?: string;
-
-  @Index()
-  @Column({ unique: true, nullable: true })
-  appleId?: string;
-
   @ManyToOne(() => Role, role => role.users, { nullable: false })
   role: Role;
 
@@ -56,12 +35,6 @@ class User extends BaseModel {
   private async hashUserPassword(): Promise<void> {
     if (this.password) {
       this.password = await passwordsHelper.generateHash(this.password);
-    }
-
-    if (this.temporaryPassword) {
-      this.temporaryPassword = await passwordsHelper.generateHash(
-        this.temporaryPassword
-      );
     }
   }
 }
